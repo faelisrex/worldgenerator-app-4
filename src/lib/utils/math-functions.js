733af1c456f95e086b48acc068610b4f4a83f336
Matt Lucero <matt_lucero@outlook.com>
2023-12-27 15:46:20 -0600

    adds shuffle array fn

--- src/lib/utils/math-functions.js
+++ src/lib/utils/math-functions.js
@@ -17,3 +17,20 @@ export function getThreeRandTable(first, second, third) {
 }
 
 export const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
+
+export function shuffleArray(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle.
+	while (currentIndex > 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+
+	return array;
+}
