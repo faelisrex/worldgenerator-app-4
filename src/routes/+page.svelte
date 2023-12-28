9f74555824544c6c946242d96dba69c5bd4db040
Matt Lucero <matt_lucero@outlook.com>
2023-12-27 18:23:00 -0600

    deletes old template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,53 +25,6 @@
 		<div class="flex flex-wrap justify-center">
 			<TownCard {world}/>
 		</div>
-
-		{#each world.settlements as settlement}
-			<div class="card p-2 my-1">
-				<h2 class="h4 text-tertiary-500 py-1">{settlement.type} of {settlement.name}</h2>
-					{#each settlement.npcs as npc}
-						<div class="m-2">
-							<p class=""><span id='{npc.id}'class="text-primary-400">{npc.getName()}</span> works as {npc.job} at {npc.building.name}</p>
-							{#each npc.quests as quest}
-								{#if quest.type === 'Bounty'}
-									<p class="">Quest: {getRandomEl(['Needs help in stopping', 'Wants to stop'])} <a href="#{quest.targetNpc.id}" class="text-primary-500 anchor">{quest.targetNpc.getName()}</a> 
-										in the {quest.targetNpc.building.name} 
-										({quest.targetNpc.type ==='Townfolk'? 'in':'near'}
-										{quest.targetNpc.location.name}) by {getRandomEl(quest.verbsSerious)} or by {getRandomEl(quest.verbsFunny)} .
-									</p>
-								{/if}
-								{#if quest.type === 'Retrieval'}
-									<p>{quest.owner.firstName} {getRandomEl(['wants to find','needs help in looking for'])} their {quest.targetItem.name}. 
-										<span class="text-xs text-primary-300">
-											It is with 
-											<a href="#{quest.targetItem.inThePocketsOf.id}" class="text-primary-500 anchor"> {quest.targetItem.inThePocketsOf.getName()}</a>
-											{quest.targetItem.inThePocketsOf.type === 'Townfolk'? 'in' : 'near'}
-											{quest.targetItem.inThePocketsOf.location.name}.
-										</span>
-									</p>
-								{/if}
-							{/each}
-						</div>
-					{/each}
-					{#if settlement.pois.length}
-					<div>
-						<h3 class="h3">Nearby Locations</h3>
-						<div class="flex">
-							{#each settlement.pois as poi}
-							<div class="card m-2 p-2">
-								<header class="card-header">{poi.name}</header>
-								{#if poi.isOccupied}
-									<section class="p-4 text-sm">
-										Occupied by {poi.npc.getName()}
-									</section>
-								{/if}
-							</div>
-							{/each}
-						</div>
-					</div>
-					{/if}
-			</div>
-		{/each}
 	</div>
 </div>
 
