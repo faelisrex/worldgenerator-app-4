3fafb8a15ad121d7349223017c6cf4195a205980
Matt Lucero <matt_lucero@outlook.com>
2023-12-27 18:23:42 -0600

    redesigns interesting people corner

--- src/components/NpcQuestList.svelte
+++ src/components/NpcQuestList.svelte
@@ -2,20 +2,13 @@
 	import { bountyDesc } from "../lib/extensions/quests/bountyQuests";
 	import { getRandomEl } from "../lib/utils/math-functions";
   export let npc;
-  export let poi;
-
-  if(poi) {
-    npc = poi.npc
-  }
 </script>
 
 {#each npc.quests as quest}
-  <div class="text-sm leading-4 tracking-tight">
-    <p class="font-semibold">Quests</p>
     {#if quest.type === 'Bounty'}
       <div class="flex">
         <p>🎯</p>
-        <p>{npc.firstName} {getRandomEl(['needs help in stopping', 'wants to stop'])} <a href="#{quest.targetNpc.id}" class="underline decoration-dotted text-error-800">{quest.targetNpc.getName()}</a> 
+        <p>{npc.firstName} {getRandomEl(['needs help in stopping', 'wants to stop'])} <a href="#{quest.targetNpc.location.id}" class="underline decoration-dotted font-semibold italic">{quest.targetNpc.getName()}</a> 
           in the {quest.targetNpc.building.name} 
           ({quest.targetNpc.type ==='Townfolk'? 'in':'near'}
           {quest.targetNpc.location.name}) by {getRandomEl(bountyDesc.bountyMethodSerious)} or by {getRandomEl(bountyDesc.bountyMethodFunny)}.
@@ -27,7 +20,7 @@
         <p>{quest.owner.firstName} {getRandomEl(['wants to find','needs help in looking for'])} their {quest.targetItem.name}. 
           <span class="text-xs">
             It is with 
-            <a href="#{quest.targetItem.inThePocketsOf.id}" class="underline decoration-dotted text-error-800"> {quest.targetItem.inThePocketsOf.getName()}</a>
+            <a href="#{quest.targetItem.inThePocketsOf.location.id}" class="underline decoration-dotted font-semibold italic"> {quest.targetItem.inThePocketsOf.getName()}</a>
             {quest.targetItem.inThePocketsOf.type === 'Townfolk'? 'in' : 'near'}
             {quest.targetItem.inThePocketsOf.location.name}.
           </span>
@@ -36,6 +29,4 @@
     {:else}
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum error nihil soluta quae velit nesciunt saepe odit deleniti magnam consequuntur maiores vel eligendi ab, excepturi perspiciatis eos nostrum sed iste?</p>
     {/if}
-
-  </div>
 {/each}
