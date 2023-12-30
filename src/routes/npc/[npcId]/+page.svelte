c71974ce898f25b741e8660fcaed772f2f091dd4
Matt Lucero <matt_lucero@outlook.com>
2023-12-30 13:34:48 -0600

    removes redundancy

--- src/routes/npc/[npcId]/+page.svelte
+++ src/routes/npc/[npcId]/+page.svelte
@@ -2,6 +2,7 @@
 	import { bountyDesc } from '../../../lib/extensions/quests/bountyQuests.js';
 	import { getRandomEl } from '../../../lib/utils/math-functions.js';
     import world from '../../../lib/world-generator-4.js';
+    import NpcQuestList from '../../../components/NpcQuestList.svelte';
     export let data;
     const npc = world.npcs.find(npc=>npc.id===data.npcId)
 </script>
@@ -23,30 +24,5 @@
     </p>
     {/each}
     <h3 class="h3">Quests</h3>
-    {#each npc.quests as quest}
-        {#if quest.type === 'Bounty'}
-            <div class="flex">
-            <p>🎯</p>
-            <p>{npc.firstName} {getRandomEl(['needs help in stopping', 'wants to stop'])} <a href="/#{quest.targetNpc.location.id}" class="underline decoration-dotted font-semibold italic">{quest.targetNpc.getName()}</a> 
-                in the {quest.targetNpc.building.name} 
-                ({quest.targetNpc.type ==='Townfolk'? 'in':'near'}
-                {quest.targetNpc.location.name}) by {getRandomEl(bountyDesc.bountyMethodSerious)} or by {getRandomEl(bountyDesc.bountyMethodFunny)}.
-            </p>
-            </div>
-        {:else if quest.type === 'Retrieval'}
-            <div class="flex">
-            <p>🔍</p>
-            <p>{quest.owner.firstName} {getRandomEl(['wants to find','needs help in looking for'])} their {quest.targetItem.name}. 
-                <span class="text-xs">
-                It is with 
-                <a href="/#{quest.targetItem.inThePocketsOf.location.id}" class="underline decoration-dotted font-semibold italic"> {quest.targetItem.inThePocketsOf.getName()}</a>
-                {quest.targetItem.inThePocketsOf.type === 'Townfolk'? 'in' : 'near'}
-                {quest.targetItem.inThePocketsOf.location.name}.
-                </span>
-            </p>
-            </div>
-        {:else}
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum error nihil soluta quae velit nesciunt saepe odit deleniti magnam consequuntur maiores vel eligendi ab, excepturi perspiciatis eos nostrum sed iste?</p>
-        {/if}
-    {/each}
+    <NpcQuestList {npc} />
 </div>
