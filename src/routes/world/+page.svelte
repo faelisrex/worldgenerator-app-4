a538acdf3b133aaae61964973def1be1895ea2e2
Matt Lucero <matt_lucero@outlook.com>
2023-12-31 01:03:52 -0600

    adds world page

---
+++ src/routes/world/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    // export let data;
+    import { storedWorld } from "$lib/stores.js";
+	import TownCard from '$lib/components/TownCard.svelte';
+
+    let world;
+    storedWorld.subscribe(value=>{
+        world = value;
+    })
+
+</script>
+
+<style>
+	.macondo {
+		font-family: 'Macondo', cursive;
+	}
+</style>
+
+<div class="m-5 grid justify-items-center">
+    <h1 class="h1 macondo">{world.name}</h1>
+    <div class="flex flex-wrap justify-center">
+        <TownCard />
+    </div>
+</div>
