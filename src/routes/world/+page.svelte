2bef67157152f1623a7903a30f5fa13a584d59cd
Matt Lucero <matt_lucero@outlook.com>
2023-12-31 01:44:48 -0600

    adds generate world button in world page

--- src/routes/world/+page.svelte
+++ src/routes/world/+page.svelte
@@ -1,13 +1,22 @@
 <script>
-    // export let data;
     import { storedWorld } from "$lib/stores.js";
+    import { generateNewWorld } from '$lib/world-generator-4';
 	import TownCard from '$lib/components/TownCard.svelte';
-
-    let world;
-    storedWorld.subscribe(value=>{
-        world = value;
-    })
-
+    export let data;
+    console.log(data);
+    function requestNewWorld(){
+        storedWorld.set(generateNewWorld(genParams));
+        return;
+    }
+    let genParams = {
+		nCities: 1,
+		nTowns: 3,
+		questChance: {
+			bounty: 0.28,
+			retrieval: 0.28
+		}
+	};
+    let world = $storedWorld;
 </script>
 
 <style>
@@ -16,6 +25,9 @@
 	}
 </style>
 
+<div class="">
+	<button class="variant-filled-success p-2 rounded" on:click={requestNewWorld}>Generate New World</button>
+</div>
 <div class="m-5 grid justify-items-center">
     <h1 class="h1 macondo">{world.name}</h1>
     <div class="flex flex-wrap justify-center">
