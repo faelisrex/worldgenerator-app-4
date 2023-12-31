2f205d96f1922549e2b935fd3b5f16b3bb24b2a2
Matt Lucero <matt_lucero@outlook.com>
2023-12-31 17:43:25 -0600

    redesigns navbar

--- src/lib/components/html/NavBar.svelte
+++ src/lib/components/html/NavBar.svelte
@@ -1,7 +1,7 @@
-<nav class="flex justify-between p-4 mb-2 bg-slate-900">
+<nav class="flex justify-between p-4 mb-2 bg-gray-200">
   <div>
     <a href="/">
-      <h3 class="h3">Matt's World Generator</h3>
+      <h3 class="h3">World Generator</h3>
     </a>
   </div>
   <ul class="flex list-none">
