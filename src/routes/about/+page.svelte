dbce288dda240cba8bc96d8c5bb3b85f2bb05791
Matt Lucero <matt_lucero@outlook.com>
2024-01-01 21:41:15 -0600

    updates acknowledgements

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,21 @@
 <script>
     // export let data;
 </script>
+<style>
+    a {
+        @apply underline decoration-dotted;
+    }
+</style>
 <div class="py-8 px-10 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
     <h2 class="h2">About page</h2>
     <h3 class="h3">About me</h3>
     Hi!
     <h3 class="h3">Acknowledgements</h3>  
-    <p>Big thanks to the helpful people on the discord server The Programmer's Hangout. Especially <b>suXin</b> (provided lots of feedback on my javascript), <b>Sebs</b> (provided the single best resource on how to write clean code,<a href="https://css-tricks.com/the-flavors-of-object-oriented-programming-in-javascript/">OOP in JS</a> ), <b>engage</b> (very engaging, also provided feedback),<b>Fin</b>,<b>TheBlue</b>, <b>snuzzers</b> and <b>sharky</b> (provided feedback on my javascript), <b>static</b> and <b>Hazy</b> (provided feedback on my ui), <b>veksen</b> (provided the best advise on flexbox, which was to understand parent-child)</p>
+    <p>Big thanks to the helpful people on the discord server The Programmer's Hangout. Especially <b>suXin</b> (provided lots of feedback on my javascript), <b>Sebs</b> (provided the single best resource on how to write clean code,<a href="https://css-tricks.com/the-flavors-of-object-oriented-programming-in-javascript/">OOP in JS</a> ), <b>engage</b> (very engaging, also provided feedback),<b>Fin</b>,<b>TheBlue</b>, <b>snuzzers</b> and <b>sharky</b> (provided feedback on my javascript), <b>static</b> and <b>Hazy</b> (provided feedback on my ui), <b>veksen</b> (provided the best advise on flexbox, which was to understand parent-child relationships).</p>
+    <br>
+    <p>On youtube: <a href="https://www.youtube.com/@JoyofCodeDev">joyofcode</a> (on svelte, holyshit his stuff is great but also very complex), <a href="https://www.youtube.com/results?search_query=huntabyte">huntabyte</a> (on svelte)</p>
+    <p>Javascript: <a href="https://javascript.info/">javascript.info</a> and the <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">mdn</a></p>
     <br><br><br>
     <p>Bye!</p>
+    <p>PS. My code lives here: <a href="https://github.com/faelisrex/worldgenerator-app-4">Github</a></p>
 </div>
