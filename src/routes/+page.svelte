5cbf72c072c2a3a62f884f9430fabb7b8d716142
Matt Lucero <matt_lucero@outlook.com>
2024-01-01 22:09:15 -0600

    adds humility to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
     <div class="">
         <div class="max-w-screen-md">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Build on top of your world's potential</h2>
-            <p class="mb-8 font-light text-gray-500 sm:text-xl dark:text-gray-400">You're the architect of your own universe, mapping out every detail with passion. Now, meet us – your creative accomplices. We're here to fill the canvas of your world, adding characters, stories, and quests that bring your creation to life. You build the world; we'll sprinkle in the magic.</p>
+            <p class="mb-8 font-light text-gray-500 sm:text-xl dark:text-gray-400">You're the architect of your own universe, mapping out every detail with passion. Now, meet us – your creative accomplices. We're here to fill the canvas of your world, adding characters, stories, and quests that bring your creation to life. You build the world; we'll sprinkle in the uhm.. sprinkles.</p>
 						<button class="bg-green-400 dark:text-black p-2 rounded" on:click={requestNewWorld}>Generate New World</button>
         </div>
     </div>
