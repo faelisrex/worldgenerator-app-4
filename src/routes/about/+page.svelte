f9c8a05394fe92dac2310e4e1b16a103f3e7f701
Matt Lucero <matt_lucero@outlook.com>
2024-01-01 21:58:11 -0600

    updates about me page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,8 +8,14 @@
 </style>
 <div class="py-8 px-10 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
     <h2 class="h2">About page</h2>
+    <h3 class="h3">About Program</h3>
+    <p>
+        This is a simple world generation program for table top rpgs. It is born out of the need to populate worlds easily so I could focus on "plot" points. This world generator, aptly named "World Generator" will generate a city and a few towns for you, populate it with npcs, give them quests, and using normal distribution personalities that fit their roles in those settlements. There are points of interests as well and may be occupied by an NPC or a bear.
+    </p>
+    <br>
     <h3 class="h3">About me</h3>
-    Hi!
+    <p>Hi!</p>
+    <br>
     <h3 class="h3">Acknowledgements</h3>  
     <p>Big thanks to the helpful people on the discord server The Programmer's Hangout. Especially <b>suXin</b> (provided lots of feedback on my javascript), <b>Sebs</b> (provided the single best resource on how to write clean code,<a href="https://css-tricks.com/the-flavors-of-object-oriented-programming-in-javascript/">OOP in JS</a> ), <b>engage</b> (very engaging, also provided feedback),<b>Fin</b>,<b>TheBlue</b>, <b>snuzzers</b> and <b>sharky</b> (provided feedback on my javascript), <b>static</b> and <b>Hazy</b> (provided feedback on my ui), <b>veksen</b> (provided the best advise on flexbox, which was to understand parent-child relationships).</p>
     <br>
